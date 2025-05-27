491b83f2000b8b099f6df828b61333d73378c2d5
Bhargava Ram <bhargav@Mac.lan>
2025-05-27 09:41:12 -0500

    Added test cases for remaining methods in expense service class

--- src/test/java/com/bhargavaram/expensetracker/api/service/ExpenseServiceTest.java
+++ src/test/java/com/bhargavaram/expensetracker/api/service/ExpenseServiceTest.java
@@ -15,6 +15,8 @@ import java.util.List;
 
 import static com.bhargavaram.expensetracker.api.model.Expense.Category.GROCERIES;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class ExpenseServiceTest {
@@ -81,6 +83,91 @@ class ExpenseServiceTest {
         assertEquals(expense.getCategory(), addedExpense.getCategory());
         assertEquals(expense.getDate(), addedExpense.getDate());
 
+    }
+
+    @Test
+    public void deleteExpenseShouldDeleteExpense(){
+
+        Mockito.when(expenseRepo.existsById(Mockito.anyInt())).thenReturn(true);
+        Mockito.doNothing().when(expenseRepo).deleteById(Mockito.anyInt());
+
+        boolean result = expenseService.deleteExpense(Mockito.anyInt());
+
+        assertTrue(result);
+        verify(expenseRepo, times(1)).deleteById(Mockito.anyInt());
+    }
+
+    @Test
+    public void deleteExpenseShouldNotDeleteIfIdDidNotExist(){
+
+        Mockito.when(expenseRepo.existsById(Mockito.anyInt())).thenReturn(false);
+
+        boolean result = expenseService.deleteExpense(Mockito.anyInt());
+
+        assertFalse(result);
+        verify(expenseRepo, times(0)).deleteById(Mockito.anyInt());
+    }
+
+    @Test
+    public void updateExpenseShouldUpdateExpense(){
+
+        Expense expense = new Expense();
+        expense.setDescription("Grocery shopping");
+        expense.setAmount(500);
+        expense.setCategory(GROCERIES);
+        expense.setDate(LocalDate.now());
+
+        int id = 3;
+
+        Mockito.when(expenseRepo.existsById(Mockito.anyInt())).thenReturn(true);
+        Mockito.when(expenseRepo.save(expense)).thenReturn(expense);
+
+        Expense updatedExpense = expenseService.updateExpense(id, expense);
+
+        assertNotNull(updatedExpense);
+        assertEquals(expense.getID(), updatedExpense.getID());
+        assertEquals(expense.getDescription(), updatedExpense.getDescription());
+        assertEquals(expense.getAmount(), updatedExpense.getAmount());
+        assertEquals(expense.getCategory(), updatedExpense.getCategory());
+        assertEquals(expense.getDate(), updatedExpense.getDate());
+    }
+
+    @Test
+    public void updateExpenseShouldAddDateIfNull(){
+
+        Expense expense = new Expense();
+        expense.setDescription("Grocery shopping");
+        expense.setAmount(500);
+        expense.setCategory(GROCERIES);
+
+        int id = 3;
+
+        Mockito.when(expenseRepo.existsById(Mockito.anyInt())).thenReturn(true);
+        Mockito.when(expenseRepo.save(expense)).thenReturn(expense);
+
+        Expense updatedExpense = expenseService.updateExpense(id, expense);
+
+        assertNotNull(updatedExpense);
+        assertNotNull(updatedExpense.getDate());
+
+    }
+
+    @Test
+    public void updateExpenseShouldNotUpdateIfIdNotExist(){
+
+        Expense expense = new Expense();
+        expense.setDescription("Grocery shopping");
+        expense.setAmount(500);
+        expense.setCategory(GROCERIES);
+        expense.setDate(LocalDate.now());
+
+        int id = 3;
+
+        Mockito.when(expenseRepo.existsById(Mockito.anyInt())).thenReturn(false);
+
+        Expense updatedExpense = expenseService.updateExpense(id, expense);
+
+        assertNull(updatedExpense);
     }
 
 }
